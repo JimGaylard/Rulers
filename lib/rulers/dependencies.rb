3785c18eee2561ca27d1038899b38be68666b19f
Jim Gaylard <jgaylard75@gmail.com>
2013-02-01 14:08:04 +1100

    if const_missing already called, return nil

--- lib/rulers/dependencies.rb
+++ lib/rulers/dependencies.rb
@@ -1,6 +1,14 @@
 class Object
   def self.const_missing(c)
-    require Rulers.to_underscore(c.to_s)
+    return nil if @calling_const_missing
+
+    @calling_const_missing = true
+    filename = Rulers.to_underscore(c.to_s)
+    require filename
+    klass = Object.const_get(c)
     Object.const_get(c)
+    @calling_const_missing = false
+
+    klass
   end
 end
